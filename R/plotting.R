2d538eed616bd896b99086b0933c367d08be2756
anna-neufeld <aneufeld@uw.edu>
2018-09-16 15:00:56 -0700

    Shortened more examples

--- R/plotting.R
+++ R/plotting.R
@@ -242,9 +242,10 @@ nodePlot <- function(model, colors = NULL) {
 #' @importFrom ggplot2 ggplot aes aes_string facet_grid geom_line geom_point scale_color_manual theme
 #' @examples
 #' \donttest{
+#' nlsySubset <- nlsySample[nlsySample$ID %in% sample(unique(nlsySample$ID), 400),]
 #' split_formula <- ~HISP + WHITE + BLACK + SEX + Num_sibs + HGC_FATHER + HGC_MOTHER
 #' tree <- splineTree(split_formula, BMI~AGE, idvar = "ID",
-#'    data = nlsySample, degree = 1, df = 3,
+#'    data = nlsySubset, degree = 1, df = 3,
 #'    intercept = TRUE, cp = 0.005)
 #' }
 #' spaghettiPlot(tree)
